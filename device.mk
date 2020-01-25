aa71cac45caf55bda5b781cd7055a836da1e6bd3
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    X00QD: Use Pixel charger animation
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -390,6 +390,11 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
