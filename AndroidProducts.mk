a69fca4b4d91f31a7567924e5fe84792e97bc9f2
Berke <berkeclb@instance-1.us-central1-a.c.quick-line-313513.internal>
2021-05-12 16:23:45 +0000

    X00QD: Corvus Bringup.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X00QD.mk
+    $(LOCAL_DIR)/corvus_X00QD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X00QD-userdebug \
-    aosp_X00QD-eng
+    corvus_X00QD-userdebug \
+    corvus_X00QD-eng
