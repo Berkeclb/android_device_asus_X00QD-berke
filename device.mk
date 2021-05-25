d4522a637203545c01057f272fca030df82da6fd
henricaodopao <allricknation@gmail.com>
2021-05-25 19:50:48 +0000

    X00QD: dont build QTI power hal it doesn't work apparently
    QTI PowerHAL: Failed to perform launch boost

--- device.mk
+++ device.mk
@@ -393,10 +393,16 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
+    android.hardware.power@1.0-service \
+    android.hardware.power@1.0-impl \
+    power.qcom \
     android.hardware.power.stats@1.0-service.mock
+    
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@1.0.vendor \
+    vendor.qti.hardware.perf@2.0.vendor
+
 
 # Protobuf
 PRODUCT_PACKAGES += \
