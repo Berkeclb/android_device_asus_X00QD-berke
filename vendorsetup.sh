a2d1a03b6ebff777a4b43188ea49536497b4a9a8
henricaodopao <allricknation@gmail.com>
2021-06-24 14:10:17 +0000

    X00QD: Rip GCC

--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
